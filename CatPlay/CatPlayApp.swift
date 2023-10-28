9d1b2a55a3b862e256df04fb4f43f3f815990cb5
Adriel Teles <adrielteles@outlook.com>
2023-10-28 18:12:03 -0300

    initial Project

---
+++ CatPlay/CatPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatPlayApp.swift
+//  CatPlay
+//
+//  Created by Adriel Teles on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CatPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
